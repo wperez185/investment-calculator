84cd2b664351d8b381284f61a60a0bab0faee379
William Perez <wperez185@yahoo.com>
2023-12-30 18:56:27 -0500

    added function to calculate cost and new component

---
+++ src/components/Results.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Results({ input }) {
+    console.log(input)
+  return (
+    <div>Results</div>
+  )
+}
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
 
-export default function UserInput() {
+export default function UserInput({ onChange, userInput }) {
+
   return (
     <section id="user-input">
         <div className="input-group">
             <label htmlFor="">Initial Investment</label>
-            <input type="number" required/>
+            <input type="number" 
+                value={userInput.initialInvestment}  
+                onChange={(event) => onChange("initialInvestment", event.target.value)}
+                required
+            />
             <label htmlFor="">Annual Investment</label>
-            <input type="number" required/>
+            <input type="number" 
+                value={userInput.annualInvestment} 
+                onChange={(event) => onChange("annualInvestment", event.target.value)}
+                required
+            />
         </div>
         <div className="input-group">
             <label htmlFor="">Expected Return</label>
-            <input type="number" required/>
+            <input type="number" 
+                value={userInput.expectedReturn} 
+                onChange={(event) => onChange("expectedReturn", event.target.value)}
+                required 
+                />
             <label htmlFor="">Duration of Investment</label>
-            <input type="number" required/>
+            <input type="number" 
+                value={userInput.duration} 
+                onChange={(event) => onChange("duration", event .target.value)}
+                required
+            />
         </div>
     </section>
   )
